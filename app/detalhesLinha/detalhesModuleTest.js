07f0a6b6dd6ae9e565653c1cb2ef99e9d74e2582
Phillipe Lopes <phillipe.alcantara.fonseca@gmail.com>
2014-12-03 14:13:36 -0200

    Terminados testes unitários do módulo de detalhes

--- app/detalhesLinha/detalhesModuleTest.js
+++ app/detalhesLinha/detalhesModuleTest.js
@@ -1,16 +1,35 @@
-'use strict';
+(function() {
+  'use strict';
 
-describe('busao.detalhes module', function() {
+  describe('busao.detalhes module', function() {
 
-  beforeEach(module('busao.detalhes'));
+    beforeEach(module('busao'));
 
-  describe('DetalhesController', function(){
+    describe('DetalhesController', function(){
+      var detalhesCtrl, detalhesService;
 
-    it('should ....', inject(function($controller) {
-      //spec body
-      var view2Ctrl = $controller('DetalhesController');
-      expect(view2Ctrl).toBeDefined();
-    }));
+      beforeEach(module('busao.detalhes'));
 
+      beforeEach(inject(function($controller, _detalhesService_, $rootScope) {
+        detalhesService = _detalhesService_;
+        detalhesCtrl = $controller('DetalhesController', {$scope: $rootScope.$new()});
+      }));
+
+      it('deveria estar definida', function() {
+        expect(detalhesCtrl).toBeDefined();
+      });
+
+      it('deveria receber os dados de detalhesService', function() {
+        var linha = {
+          nome: "0806 - T. JACARAIPE / NOVA ALMEIDA VIA PARQUE GAIVOTAS (CIRCULAR)",
+          numero: "0806"
+        };
+
+        detalhesService.setLinhaOnibus(linha);
+        expect(detalhesCtrl.getLinhaAtual()).toBeDefined();
+        expect(detalhesCtrl.getLinhaAtual()).toBe(linha);
+      });
+
+    });
   });
-});
+})();
